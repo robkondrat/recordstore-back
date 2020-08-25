422dee95227b06290f3343e3a419927246c911c8
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-08-25 17:28:21 -0500

    updated users, authorize cors, updated routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
+
   def not_authorized
     render json: { error: 'Not Authorized' }, status: :unauthorized
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'signin/create'
+
   namespace :api do
     namespace :v1 do
-      resources :records
-      resources :artists
+      resource :artists do
+        resources :records
+      end
     end
   end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
