cc4a107caa3897b96d312dc0c9d8e2ca5a148c47
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-08-25 17:14:18 -0500

    signup endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  private
+
+  def not_authorized
+    render json: { error: 'Not Authorized' }, status: :unauthorized
+  end
 end
